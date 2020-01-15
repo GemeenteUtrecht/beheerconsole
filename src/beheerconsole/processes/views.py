abe74442dbb0c3f33e90ee38190f46b983803cdf
annashamray <anna.shamray@maykinmedia.nl>
2020-01-15 10:48:25 +0100

    get xml for bpmn via request instead of hidden html element

--- src/beheerconsole/processes/views.py
+++ src/beheerconsole/processes/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponse
+from django.views import View
 from django.views.generic import DetailView
 
 from django_filters.views import FilterView
@@ -15,3 +17,9 @@ class ProcessListView(FilterView):
 class ProcessDetailView(DetailView):
     model = Process
     template_name = "processes/process_detail.html"
+
+
+class ProcessBpmnView(View):
+    def get(self, request, pk):
+        process = Process.objects.get(pk=pk)
+        return HttpResponse(process.xml(), content_type="text/xml")
